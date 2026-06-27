196a6e9d5e28e870f77cae17135a2f0662299233
fl9mdasif <freelancer.mdasifofficial@gmail.com>
2026-06-27 16:13:21 +0600

    ready for deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,8 +28,21 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
-        {children}
+      <body className="min-h-full flex flex-col bg-[#f8f9fa] text-[#171717]">
+        <main className="flex-1">{children}</main>
+        <footer className="w-full py-5 text-center text-xs text-gray-500 border-t border-[#e5e7eb] bg-white mt-auto select-none">
+          <p>
+            © {new Date().getFullYear()} All rights reserved{' '}
+            <a
+              href="https://dev-mdasif-portfolio.vercel.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="font-semibold text-black hover:underline transition-colors"
+            >
+              dev_asif
+            </a>
+          </p>
+        </footer>
         <Toaster position="bottom-right" richColors />
       </body>
     </html>
